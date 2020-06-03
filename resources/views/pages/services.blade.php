4c94a79f47ab2bd221485fa105d2343cd856d689
marshlobo <32623047+marshlobo@users.noreply.github.com>
2020-06-03 16:14:20 +0530

    extends layout and section is added template is used from app.blade.php

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>{{config('app.name','LSAPP')}}</title>     
-    </head>
-    <body>
-       <h1> Welcome to services</h1>
-       <p> this is the services file </p>
-    </body>
-</html>
+@extends('layouts.app')
+@section('content')
+<h1> Welcome to services</h1>
+<p> this is the services file </p>   
+@endsection
+   
+

1cae33b959a18fa25de1ebd80961dbcb99fb5262
marshlobo <32623047+marshlobo@users.noreply.github.com>
2020-06-03 12:19:52 +0530

    route to function index in controller PagesController

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 // Route::get('/hello', function () {
 //     return 'hello world';
@@ -26,4 +26,6 @@ Route::get('/', function () {
  Route::get('/about', function() {
     return view('pages.about');
  });
+
+ Route::get('/','Pagescontroller@index');
 

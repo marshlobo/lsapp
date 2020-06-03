d6558c2739966739cc80c20b46b75a08696a083a
marshlobo <32623047+marshlobo@users.noreply.github.com>
2020-06-03 12:57:25 +0530

    created a service router

--- routes/web.php
+++ routes/web.php
@@ -29,4 +29,5 @@ use Illuminate\Support\Facades\Route;
 
  Route::get('/','Pagescontroller@index');
  Router::get('/about','PagesController@about');
+ Router::get('/services','PagesController@service');
 

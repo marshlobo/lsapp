dcc5168178d8a6b878e0794e56bba8674d71ad63
marshlobo <32623047+marshlobo@users.noreply.github.com>
2020-06-03 13:04:59 +0530

    bugg fixed

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,6 @@ use Illuminate\Support\Facades\Route;
 //  });
 
  Route::get('/','Pagescontroller@index');
- Router::get('/about','PagesController@about');
- Router::get('/services','PagesController@services');
+ Route::get('/about','PagesController@about');
+ Route::get('/services','PagesController@services');
 

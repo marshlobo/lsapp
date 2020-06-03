e03a1b6faaa2c09b9cd7b149dfae8a9f63634046
marshlobo <32623047+marshlobo@users.noreply.github.com>
2020-06-03 16:17:51 +0530

    bugg fixed

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('contents')
+@section('content')
 <h1> Welcome to About</h1>
 <p> this is the about file </p>  
 @endsection
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,4 +1,4 @@
-@extends('lauouts.app')
+@extends('layouts.app')
 
 @section('content') 
        <h1> Welcome to laravel</h1>

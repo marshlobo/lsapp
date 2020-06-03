cd598ea70eab1a5e40c6f9d02ca04e570aa150eb
marshlobo <32623047+marshlobo@users.noreply.github.com>
2020-06-03 12:45:45 +0530

    sample template for about page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,1 +1,12 @@
-about
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>{{config('app.name','LSAPP')}}</title>     
+    </head>
+    <body>
+       <h1> Welcome to About</h1>
+       <p> this is the about file </p>
+    </body>
+</html>

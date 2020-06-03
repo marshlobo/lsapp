d3b0376429a3f6e83381cea21ad39b822f6ab343
marshlobo <32623047+marshlobo@users.noreply.github.com>
2020-06-03 11:58:54 +0530

    added a fuction for about router with get method' '

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/hello', function () {
-    return 'hello world';
-});
+
+// Route::get('/hello', function () {
+//     return 'hello world';
+// });
+ Route::get('/about', function() {
+    return view('pages.about');
+ });
